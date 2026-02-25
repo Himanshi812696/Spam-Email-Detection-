57755dd54d0f2360800c73361bed1e87b56dca94
Your Name <you@example.com>
2026-02-25 12:33:03 +0530

    Updated project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+from io import BytesIO
 import pickle
 from joblib import load as joblib_load
 import os
@@ -7,6 +8,13 @@ import warnings
 warnings.filterwarnings('ignore', category=UserWarning)
 
 app = Flask(__name__)
+
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,6 +32,14 @@ try:
 except:
     with open(vectorizer_path, "rb") as f:
         vectorizer = pickle.load(f)
+
+@app.route("/favicon.ico")
+def favicon():
+    favicon_svg = '''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+        <rect width="100" height="100" fill="#667eea" rx="20"/>
+        <text x="50" y="65" font-size="70" text-anchor="middle" fill="white">📧</text>
+    </svg>'''
+    return send_file(BytesIO(favicon_svg.encode()), mimetype="image/svg+xml")
 
 @app.route("/")
 def home():
